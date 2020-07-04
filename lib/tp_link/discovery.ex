df72060e074ccb6df8501c904a9aa9aaef73cff2
Brian Alexander <balexand@gmail.com>
2020-07-04 17:57:15 -0600

    successfully sent discovery message

--- lib/tp_link/discovery.ex
+++ lib/tp_link/discovery.ex
@@ -1,8 +1,11 @@
 defmodule TpLink.Discovery do
   use GenServer
 
-  @multicast_ip {224, 1, 1, 1}
-  @port 49999
+  # @multicast_ip {224, 1, 1, 1}
+  @multicast_ip {255, 255, 255, 255}
+  @port 9999
+
+  @discovery_msg Base.decode64!("0PKB+Iv/mvfV75S20bTAn+yV5o/hh+jK8Iv2i6eF4I3onPmLqZPoyq3IvOOR9JX5jeSJ7M70j/KPo4Hyn/6M+JT9m/7QudaijO+A7YDvga/Kp8K206GDucLgh+KWybvev9OnzqPG5N6l2KXY")
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, :ok, opts)
@@ -18,9 +21,10 @@ defmodule TpLink.Discovery do
       :binary,
       active: 10,
       add_membership: {@multicast_ip, {0, 0, 0, 0}},
+      broadcast: true,
       ip: {0, 0, 0, 0},
       # multicast_if: {0, 0, 0, 0},
-      multicast_loop: true,
+      multicast_loop: false,
       multicast_ttl: 1,
       reuseaddr: true
     ]
@@ -44,7 +48,7 @@ defmodule TpLink.Discovery do
 
   @impl GenServer
   def handle_call(:discover, _form, %{socket: socket} = state) do
-    :ok = :gen_udp.send(socket, @multicast_ip, @port, ["message x"])
+    :ok = :gen_udp.send(socket, @multicast_ip, @port, [@discovery_msg])
 
     {:reply, :ok, state}
   end
@@ -53,7 +57,7 @@ defmodule TpLink.Discovery do
   def handle_info({:udp, socket, ip, port, data}, state) do
     # when we popped one message we allow one more to be buffered
     :inet.setopts(socket, active: 1)
-    IO.inspect(["received", ip, port, data])
+    IO.inspect(["received", ip, port, data == @discovery_msg])
     {:noreply, state}
   end
 end
