cb5326fd609c7b50edf43dc589ed79e672030162
Brian Alexander <balexand@gmail.com>
2022-02-12 13:40:15 -0700

    drop support for legacy Elixir

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule TpLink.MixProject do
       app: :tp_link,
       description: "Interact with TP-Link/Kasa devices via the cloud or local network.",
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: [
