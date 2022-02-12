eb364df91ea4b346f08d1bc5197fc89671534052
Brian Alexander <balexand@gmail.com>
2022-02-11 20:01:38 -0700

    Update tp_link.ex

--- lib/tp_link.ex
+++ lib/tp_link.ex
@@ -8,6 +8,9 @@ defmodule TpLink do
 
   defdelegate call(device, command), to: TpLink.Device
 
+  @doc """
+  Creates a device struct for a device that is accessed through the cloud.
+  """
   def cloud_device(%Session{} = session, device_id) when is_binary(device_id) do
     %CloudDevice{device_id: device_id, session: session}
   end
